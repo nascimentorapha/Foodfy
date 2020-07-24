e7b0e0c40d00d2975bf4a2f1bc377c55dbabbfe9
nascimentorapha <nascimento.rapha@hotmail.com>
2020-07-24 16:51:33 -0300

    configured parameters and routes to migrate to routes.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
-const dados = require("./public/data")
+const routes = require('./routes')
+const dados = require("./public/data.json")
 
 server.use(express.static('public'))
 server.use(express.static('content'))
@@ -17,16 +18,16 @@ nunjucks.configure("views", {
 })
 
 server.get("/", function(req, res){
-    return res.render("index", { content: dados })
+    return res.render("index", { content: dados.recipes })
 })
 
 server.get('/recipe', function(req, res){
-    return res.render("recipe", { content: dados })
+    return res.render("recipe", { content: dados.recipes })
 })
 
 server.get("/recipe/:index", function (req, res) {
     const recipeIndex = req.params.index;
-    const recipe = dados[recipeIndex]; //Array de receitas carregado data.js
+    const recipe = dados.recipes[recipeIndex]; //Array de receitas carregado data.json
 
     return res.render("full-recipe", { recipe })
 })
