1086fb71add1403937f03190c492e489dff506f7
nascimentorapha <nascimento.rapha@hotmail.com>
2020-07-27 16:54:45 -0300

    included admin routes

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const routes = express.Router()
+const dados = require("./public/data.json")
+const recipes = require('./public/controllers/admin.js')
+
 
 routes.get("/", function(req, res){
     return res.render("index", { content: dados.recipes })
@@ -20,5 +23,13 @@ routes.get("/about", function(req, res){
     return res.render("about")
 })
 
+routes.get("/admin/recipes", recipes.index)
+routes.get("/admin/recipes/create", recipes.create)
+routes.get("/admin/recipes/:id", recipes.show)
+routes.get("/admin/recipes/:id/edit", recipes.edit)
+
+routes.post("/admin/recipes", recipes.post)
+routes.put("/admin/recipes", recipes.put)
+routes.delete("/admin/recipes", recipes.delete)
 
 module.exports = routes
