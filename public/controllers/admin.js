f9d70b73ca71d1297ba55d72bbbc3de63d80351b
nascimentorapha <nascimento.rapha@hotmail.com>
2020-07-29 16:51:30 -0300

    configured show and edit function

--- public/controllers/admin.js
+++ public/controllers/admin.js
@@ -4,7 +4,7 @@ const data = require('../data.json')
 const { resolveNaptr } = require('dns')
 
 exports.index = function(req, res) {
-    return res.render('admin/index', {content: data.recipes})
+    return res.render('admin/index', {recipe: data.recipes})
 }
 
 exports.create = function (req, res) {
@@ -12,11 +12,37 @@ exports.create = function (req, res) {
 }
 
 exports.show = function(req, res) {
-    return res.render('admin/details', {content: data.recipes})
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find(function(recipe){
+        return recipe.id == id
+    })
+
+    if (!foundRecipe)
+        return res.send("Recipe not found")
+    
+    const recipe ={
+        ...foundRecipe, //coloca todos os campos do recipe
+    }
+
+    return res.render('admin/details', {recipe})
 }
 
 exports.edit = function(req, res) {
-    return res.render('admin/edit')
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find(function(recipe){
+        return recipe.id == id
+    })
+
+    if (!foundRecipe)
+        return res.send("Recipe not found")
+    
+    const recipe ={
+        ...foundRecipe, //coloca todos os campos do recipe
+    }
+
+    return res.render('admin/edit', {recipe})
 }
 
 exports.post = function(req, res){
